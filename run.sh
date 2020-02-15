0d26a625f53417bd8a5406401798d6260fb82b3b
david <david-khala@hotmail.com>
2020-02-15 09:48:57 +0800

    [TODO] traefik chaos

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ up() {
     ./traefik --configFile=config/traefik.sample.toml
 }
 cli() {
-    ./traefik --entryPoints.web.address=:8081 --providers.file.filename=config/dynamic_conf.yml --api.insecure=true
+    ./traefik --entryPoints.web.address=:8081 --providers.file.filename=config/dynamic_conf.yml
 }
 
 $1
